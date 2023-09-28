7a400791b0197c42154177f0f69687fcb0d12cbf
Ritvik Vasan <ritvik.vasan@prd-aics-dcp-038.corp.alleninstitute.org>
2023-09-27 17:04:28 -0700

    add basic model that outputs 2d loss + embedding head

--- pointcloudutils/networks/__init__.py
+++ pointcloudutils/networks/__init__.py
@@ -2,3 +2,5 @@ from .local_decoder import LocalDecoder
 from .equiv_transformer import EquivTransformer
 from .point_m2ae import Point_M2AE
 from .point_mae import Point_MAE
+from .i2p_mae import I2P_MAE
+from .basic_model import BasicModel
